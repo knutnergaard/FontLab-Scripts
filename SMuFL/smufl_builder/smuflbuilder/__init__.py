a546ee7943a7c4fe4cca247c42da46a814d1b083
Knut Nergaard <knut.nergaard@gmail.com>
2021-05-03 21:29:09 +0200

    Update __init__.py

--- SMuFL/smufl_builder/smuflbuilder/__init__.py
+++ SMuFL/smufl_builder/smuflbuilder/__init__.py
@@ -1,6 +1,6 @@
 """SMuFLbuilder is a tool to build glyph components in SMuFL fonts.
 
 SMuFLbuilder is currently only compatible with FontLab Studio 5 and Python 2.
-Version 0.1
+Version 0.2
 """
 # (c) 2021 by Knut Nergaard.
