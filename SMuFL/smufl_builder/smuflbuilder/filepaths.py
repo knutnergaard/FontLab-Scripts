9cf0d99a8f503750328589780b0801180e8853c0
Knut Nergaard <knut.nergaard@gmail.com>
2021-05-03 21:42:06 +0200

    Update filepaths.py

--- SMuFL/smufl_builder/smuflbuilder/filepaths.py
+++ SMuFL/smufl_builder/smuflbuilder/filepaths.py
@@ -1,5 +1,5 @@
-defaults = '/Users/knutnergaard/Library/Application Support/FontLab/Studio 5/Macros/System/Modules/smuflbuilder/defaults.ini'
+defaults = '~/Library/Application Support/FontLab/Studio 5/Macros/System/Modules/smuflbuilder/defaults.ini'
 
 
 # Change the filepath below to match the location of smuflbuilder_settings.ini.
-user = '/Users/knutnergaard/kode/python/smufl/smuflbuilder/new/SMuFLbuilder Settings/smuflbuilder.ini'
+user = '~/Documents/SMuFLbuilder Settings/smuflbuilder.ini'
